511578858298cc82a9512f9610622213d16529d4
Sol Astri <me@danielsol.dev>
2024-08-22 17:05:20 +0300

    first logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import TeapotLogo from './images/teapot.svg';
 
 const Header = () => (
-    <header className="bg-tea-black text-tea-white py-20">
-        <div className="container mx-auto px-4">
-            <h1 className="text-6xl font-bold mb-4">Thoughts and Tea | 思茶</h1>
-            <p className="text-2xl font-light ml-4 border-l-4 border-tea-red pl-4">
-                University Club for Tea Enthusiasts and Thinkers
-            </p>
+    <header className="bg-tea-black text-tea-white py-20 relative">
+        <div className="container mx-auto px-4 py-6 flex justify-between items-center">
+            <div>
+                <h1 className="text-4xl font-serif mb-2">Thoughts and Tea | 思茶</h1>
+                <p className="text-lg text-blue-300 border-l-4 border-red-500 pl-3">
+                    University Club for Tea Enthusiasts and Thinkers
+                </p>
+            </div>
+            <img
+                src={TeapotLogo}
+                alt="Teapot Logo"
+                className="w-32 h-32"
+            />
         </div>
     </header>
 );
